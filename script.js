bf3f55d3f69be4b30a5a9298af5c0873a7503b04
Omar Rafat <omarrafat613@gmail.com>
2024-11-27 18:49:06 +0200

    login and register and about and new photos for the site

--- script.js
+++ script.js
@@ -89,19 +89,3 @@ const slider = () => {
   });
 };
 slider();
-
-/// MAP
-
-var map = L.map('map').setView([52.3829933, 4.7402684], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  zoomControl: false,
-  attributionControl: false,
-  boxZoom: false,
-  dragging: false,
-}).addTo(map);
-
-L.marker([52.3829933, 4.7402684])
-  .addTo(map)
-  .bindPopup('Our Location')
-  .openPopup();
